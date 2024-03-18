2614d9c2c34f7e49afb1b2a6f9240ac0ab4e47fd
Rodolpho Becker <becker.rodolpho@gmail.com>
2024-03-18 15:27:36 -0300

    [FEAT] Home page implemented

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { Container, Box } from "@mui/material";
-import Header from '../Header'
+import Header from "../Header";
 import Head from "next/head";
 import { useRouter } from "next/router";
 interface Props {
@@ -18,16 +18,22 @@ const AppLayout = ({ children }: Props) => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
-			<Container maxWidth="xl" sx={{ paddingTop: "100px" }}>
-				<Box pb="50px">{children}</Box>
-			</Container>
-			<Box
-				height="15px"
-				width="100%"
-				position="fixed"
-				bottom="0"
-				sx={{ background: "rgb(162, 7, 41)", zIndex: "9999" }}
-			/>
+			{router.route === "/" ? (
+				<Box>{children}</Box>
+			) : (
+				<>
+					<Container maxWidth="xl" sx={{ paddingTop: "100px" }}>
+						<Box pb="50px">{children}</Box>
+					</Container>
+					<Box
+						height="15px"
+						width="100%"
+						position="fixed"
+						bottom="0"
+						sx={{ background: "rgb(162, 7, 41)", zIndex: "9999" }}
+					/>
+				</>
+			)}
 		</>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,69 @@
-import MapChart from "@/components/Maps/MapChart";
-import { Box } from "@mui/material";
-import React from "react";
+import { Button, Stack, Typography } from "@mui/material";
+import { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const index = () => {
+const Home: NextPage = () => {
+	const router = useRouter();
+
+	const handleButtonClick = (url: string) => {
+		router.push(url);
+	};
+
 	return (
-		<Box>
-			<MapChart />
-		</Box>
+		<Stack
+			height="100vh"
+			justifyContent="center"
+			display="flex"
+			flexDirection="row"
+		>
+			<Stack
+				flexBasis="50%"
+				height="100%"
+				className="white-background"
+				sx={{ alignItems: "center", justifyContent: "center" }}
+			>
+				<Image
+					src={"/background-collinson.png"}
+					height={400}
+					width={400}
+					alt="logo-collinson"
+				/>
+			</Stack>
+			<Stack
+				flexBasis="50%"
+				height="100%"
+				flexDirection="column"
+				justifyContent="center"
+				className="collinson-red-background"
+			>
+				<Typography variant="h5" sx={{ textAlign: "center", width: "100%" }}>
+					Please select one of the following options to see more about
+				</Typography>
+				<Stack
+					display="flex"
+					flexDirection="row"
+					justifyContent="center"
+					width="100%"
+					mt="100px"
+					columnGap={5}
+				>
+					<Button
+						variant="contained"
+						color="inherit"
+						className="collinson-pink-background collinson-red-text"
+						onClick={() => handleButtonClick("/visits")}
+					>
+						Visits
+					</Button>
+					<Button variant="contained" color="info" disabled>
+						Future Section
+					</Button>
+				</Stack>
+			</Stack>
+		</Stack>
 	);
 };
 
-export default index;
+export default Home;
